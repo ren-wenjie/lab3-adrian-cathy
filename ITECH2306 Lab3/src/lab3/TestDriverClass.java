51db1dcbf488e2f37906c6400985dac1ec530f3a
Adrian Page <adrianpage@students.federation.edu.au>
2019-03-20 12:46:59 +1100

    Updated test

--- ITECH2306 Lab3/src/lab3/TestDriverClass.java
+++ ITECH2306 Lab3/src/lab3/TestDriverClass.java
@@ -10,25 +10,26 @@ package lab3;
 public class TestDriverClass {
 
 	/**
-	 * @param args
+	 * @param Cathy & Adrian
 	 */
-	
+	 
 	public static void main(String[] args) {
-		// create test case 1 - a person owning a pet dog
-		Person p1 = new Person( "25 somewhere Street","Kathleen","2000");
-		Animal myPet = new Animal("CockerSpaniel","Fido");
-		p1.addAPet(myPet);
-		System.out.println("I have a person object : " + p1.toString());
-		
-		// create test case 2 - another person owing a pet dog
-		Person p2 = new Person("12 curly avenue","Fred", "3350");
-		Animal anotherPet = new Animal("Labrador","Sue");
-		Course c2 = new Course("Test Course", 10,1000.0); //Test course with too many days
-		p2.addAPet(anotherPet);
-		p2.addCourse(c2);
-		System.out.println("I have a second person object : " + p2 + c2);
-		
-
+	 // create test case 1 - a person owning a pet dog
+	 Animal myPet = new Animal("CockerSpaniel", "Fido");
+	 Person p1 = new Person("Cathy", "25 Boak Avenue,", "Buninyong VIC ", "3357", myPet);
+	 p1.addAPet(myPet);
+	 System.out.println("I have a person object : " + p1.toString());
+	 
+	 // create test case 2 - another person owing a pet dog
+	 Person p2 = new Person("Adrian", "12 curly avenue", " Mount Clear VIC", "3350", myPet);
+	 Animal anotherPet = new Animal("Labrador", "Sue");
+	 p2.addAPet(anotherPet);
+	 System.out.println("I have a second person object : " + p2);
+	 
+	 Course myCourse = new Course("Java", 10, 1000.00);
+	 Person p3 = new Person("Anson", "25 Boak Avenue", " Buninyong VIC", "3357", myCourse);
+	 p2.addACourse(myCourse);
+	 System.out.println("I have a third person object : " + p3.toString());
 	}
 
 }
